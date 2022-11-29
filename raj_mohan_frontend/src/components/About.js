31b0bc2b7c7c114036f0095056e5cc362c177ff3
Raj Mohan <68589327+rajmohanreddy774@users.noreply.github.com>
2022-11-29 15:58:14 +0530

    updating About.js

--- raj_mohan_frontend/src/components/About.js
+++ raj_mohan_frontend/src/components/About.js
@@ -6,7 +6,7 @@ const About = ({ item }) => {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 400,
+    width: 700,
     bgcolor: "#b5ebda",
     boxShadow: 24,
     p: 4,
@@ -16,24 +16,23 @@ const About = ({ item }) => {
       <Typography id="modal-modal-title" variant="h6" component="h2">
         {item.capsule_serial}
       </Typography>
-      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+      <Typography id="modal-modal-description" sx={{ mt: 5 }}>
         Launch at: {moment(item.original_launch).format("YYYY-MM-DD")}
       </Typography>
       <Typography id="modal-modal-title" variant="h6" component="h2">
-        Missions :{" "}
         {item.missions.map((e) => {
           return (
             <Typography id="modal-modal-description">Name: {e.name}</Typography>
           );
         })}
       </Typography>
-      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+      <Typography id="modal-modal-description" sx={{ mt: 5 }}>
         Description: {item.details}
       </Typography>
-      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+      <Typography id="modal-modal-description" sx={{ mt: 5 }}>
         Type: {item.type}
       </Typography>
-      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+      <Typography id="modal-modal-description" sx={{ mt: 5 }}>
         Landings: {item.landings}
       </Typography>
     </Box>
